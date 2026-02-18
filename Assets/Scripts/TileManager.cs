a34157762258c2152f37df094c3209d4edbfe7ed
ProfAlves-SG <guilhermesilva.altodalapa@supergeeks.com.br>
2026-02-18 14:10:12 -0300

    Finish tile manager

---
+++ Assets/Scripts/TileManager.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+    [SerializeField] private GameObject[] tilePrefab;
+    [SerializeField] private float zDistanceToSpawn;
+    [SerializeField] private float numberOfTiles;
+    
+    [SerializeField] private float tileLength;
+    [SerializeField] private Transform playerTransform;
+    
+    private List<GameObject> activeTiles = new List<GameObject>();
+    
+    private void Start()
+    {
+        for (int i = 0; i < numberOfTiles; i++)
+        {
+            if (i == 0)
+            {
+                SpawnTile(0);
+            }
+            else
+            {
+                SpawnTile(Random.Range(0, tilePrefab.Length));
+            }
+        }
+    }
+
+    private void Update()
+    {
+        if(playerTransform.position.z - 50 > zDistanceToSpawn - (numberOfTiles * tileLength))
+        {
+            SpawnTile(Random.Range(0, tilePrefab.Length));
+            DeleteTile();
+        }
+    }
+    
+    private void SpawnTile(int tileIndex)
+    {
+        GameObject tile = Instantiate(tilePrefab[tileIndex], transform.forward * zDistanceToSpawn, transform.rotation);
+        activeTiles.Add(tile);
+        zDistanceToSpawn += tileLength;
+    }
+
+    private void DeleteTile()
+    {
+        Destroy(activeTiles[0]);
+        activeTiles.RemoveAt(0);
+    }
+}
